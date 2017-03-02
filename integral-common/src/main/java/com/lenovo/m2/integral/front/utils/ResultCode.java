04d696872794332b2a5f72cc4f1dbdcabcdbfddc
shaoyh2 <shaoyh2@lenovo.com>
2017-03-02 13:52:09 +0800

    添加参数校验

---
+++ integral-common/src/main/java/com/lenovo/m2/integral/front/utils/ResultCode.java
@@ -0,0 +1,12 @@
+package com.lenovo.m2.integral.front.utils;
+
+/**
+ * Created by admin on 2017/3/2.
+ */
+public class ResultCode {
+
+    public static final String PARAMS_FAIL = "2001";//参数不正确
+
+    public static final String FAIL = "8888";//系统异常
+
+}
--- integral-web/src/main/java/com/lenovo/m2/integral/front/controller/CouponAndIntegralController.java
+++ integral-web/src/main/java/com/lenovo/m2/integral/front/controller/CouponAndIntegralController.java
@@ -1,11 +1,12 @@
 package com.lenovo.m2.integral.front.controller;
 
+import com.lenovo.m2.arch.framework.domain.Money;
 import com.lenovo.m2.arch.framework.domain.PageModel2;
 import com.lenovo.m2.arch.framework.domain.PageQuery;
 import com.lenovo.m2.arch.framework.domain.RemoteResult;
+import com.lenovo.m2.integral.front.utils.ResultCode;
 import com.lenovo.m2.integral.soa.api.CouponAndIntegralInfoService;
 import com.lenovo.m2.integral.soa.api.ExchangeCouponRecordService;
-import com.lenovo.m2.integral.soa.api.IntegralResultCode;
 import com.lenovo.m2.integral.soa.domain.CouponAndIntegralInfo;
 import com.lenovo.m2.integral.soa.utils.JacksonUtil;
 import org.slf4j.Logger;
@@ -14,6 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by admin on 2017/2/22.
@@ -36,11 +41,19 @@ public class CouponAndIntegralController {
         LOGGER.info("addIntegralBindingCoupon Start:"+couponId+";"+memberId+";"+integralNum+";"+state);
 
         RemoteResult remoteResult = new RemoteResult();
+
         try {
+            if (couponId==null || memberId==null || integralNum==null || state==null){
+                remoteResult.setResultCode(ResultCode.PARAMS_FAIL);
+                remoteResult.setResultMsg("参数不正确！");
+                LOGGER.info("addIntegralBindingCoupon End:" + JacksonUtil.toJson(remoteResult));
+                return remoteResult;
+            }
+
             remoteResult = couponAndIntegralInfoService.addCouponInfo(couponId, memberId, integralNum, state);
         }catch (Exception e){
             remoteResult.setResultMsg("系统异常");
-            remoteResult.setResultCode(IntegralResultCode.FAIL);
+            remoteResult.setResultCode(ResultCode.FAIL);
             LOGGER.error(e.getMessage(),e);
         }
         LOGGER.info("addIntegralBindingCoupon End:"+ JacksonUtil.toJson(remoteResult));
@@ -55,10 +68,17 @@ public class CouponAndIntegralController {
 
         RemoteResult<CouponAndIntegralInfo> remoteResult = new RemoteResult<CouponAndIntegralInfo>();
         try {
+            if (couponId==null){
+                remoteResult.setResultCode(ResultCode.PARAMS_FAIL);
+                remoteResult.setResultMsg("参数不正确");
+                LOGGER.info("getCouponInfo End:" + JacksonUtil.toJson(remoteResult));
+                return remoteResult;
+            }
+
             remoteResult = couponAndIntegralInfoService.getCouponInfo(couponId);
         }catch (Exception e){
             remoteResult.setResultMsg("系统异常");
-            remoteResult.setResultCode(IntegralResultCode.FAIL);
+            remoteResult.setResultCode(ResultCode.FAIL);
             LOGGER.error(e.getMessage(),e);
         }
         LOGGER.info("getCouponInfo End:"+ JacksonUtil.toJson(remoteResult));
@@ -68,10 +88,52 @@ public class CouponAndIntegralController {
     //查询优惠券和积分绑定记录加分页
     @RequestMapping("/getCouponInfoByPage")
     @ResponseBody
-    public RemoteResult getCouponInfoByPage(PageQuery pageQuery, CouponAndIntegralInfo couponAndIntegralInfo){
-        LOGGER.info("getCouponInfoByPage Start:"+JacksonUtil.toJson(pageQuery)+";"+JacksonUtil.toJson(couponAndIntegralInfo));
+    public RemoteResult getCouponInfoByPage(PageQuery pageQuery, CouponAndIntegralInfo couponAndIntegralInfo,HttpServletRequest request){
+        LOGGER.info("getCouponInfoByPage Start:"+JacksonUtil.toJson(pageQuery)+";"+JacksonUtil.toJson(couponAndIntegralInfo)+";"+JacksonUtil.toJson(request));
 
         RemoteResult<PageModel2<CouponAndIntegralInfo>> remoteResult = new RemoteResult<PageModel2<CouponAndIntegralInfo>>();
+        try {
+            //参数处理
+            String money = request.getParameter("money");
+            if (money!=null){
+                /*BigDecimal decimal = new BigDecimal(Integer.parseInt(money));
+                Money money1 = new Money();
+                money1.setAmount(decimal);
+                couponAndIntegralInfo.setCouponMoney(money1);*/
+                Money cny = new Money(Integer.parseInt(money), "CNY");
+                couponAndIntegralInfo.setCouponMoney(cny);
+            }
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String fromtime = request.getParameter("usestarttime");
+            if (fromtime!=null){
+                Date parse = sdf.parse(fromtime);
+                couponAndIntegralInfo.setFromtime(parse);
+            }
+            String totime = request.getParameter("useendtime");
+            if (totime!=null){
+                Date parse = sdf.parse(totime);
+                couponAndIntegralInfo.setTotime(parse);
+            }
+            String gainstarttime = request.getParameter("gainstarttime");
+            if (gainstarttime!=null){
+                Date parse = sdf.parse(gainstarttime);
+                couponAndIntegralInfo.setTotime(parse);
+            }
+            String gainendtime = request.getParameter("gainendtime");
+            if (gainendtime!=null){
+                Date parse = sdf.parse(gainendtime);
+                couponAndIntegralInfo.setTotime(parse);
+            }
+        }catch (Exception e){
+            remoteResult.setResultCode(ResultCode.PARAMS_FAIL);
+            remoteResult.setResultMsg("参数不正确");
+            LOGGER.error(e.getMessage(),e);
+            LOGGER.info("getCouponInfo End:" + JacksonUtil.toJson(remoteResult));
+            return remoteResult;
+        }
+
+
         try {
             if (pageQuery.getPageSize()==0){
                 pageQuery.setPageSize(10);
@@ -82,20 +144,36 @@ public class CouponAndIntegralController {
             remoteResult = couponAndIntegralInfoService.getCouponInfoByPage(pageQuery, couponAndIntegralInfo);
         }catch (Exception e){
             remoteResult.setResultMsg("系统异常");
-            remoteResult.setResultCode(IntegralResultCode.FAIL);
+            remoteResult.setResultCode(ResultCode.FAIL);
             LOGGER.error(e.getMessage(),e);
         }
         LOGGER.info("getCouponInfoByPage End:"+ JacksonUtil.toJson(remoteResult));
         return remoteResult;
     }
 
+    //修改绑定记录
     @RequestMapping("/updateCouponInfo")
     @ResponseBody
     public RemoteResult updateCouponInfo(String couponId,String memberId, Integer integralNum, Integer state){
-        RemoteResult remoteResult = couponAndIntegralInfoService.updateCouponInfo(couponId, memberId, integralNum, state);
+        LOGGER.info("updateCouponInfo Start:"+couponId+";"+memberId+";"+integralNum+";"+state);
+
+        RemoteResult remoteResult = new RemoteResult();
+        try {
+            if (couponId==null || memberId==null || integralNum==null || state==null){
+                remoteResult.setResultCode(ResultCode.PARAMS_FAIL);
+                remoteResult.setResultMsg("参数不正确");
+                LOGGER.info("updateCouponInfo End:"+ JacksonUtil.toJson(remoteResult));
+                return remoteResult;
+            }
+
+            remoteResult = couponAndIntegralInfoService.updateCouponInfo(couponId, memberId, integralNum, state);
+        }catch (Exception e){
+            remoteResult.setResultMsg("系统异常");
+            remoteResult.setResultCode(ResultCode.FAIL);
+            LOGGER.error(e.getMessage(),e);
+        }
+        LOGGER.info("updateCouponInfo End:"+ JacksonUtil.toJson(remoteResult));
         return remoteResult;
     }
-
-
 
 }
